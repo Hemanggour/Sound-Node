b01145d1a82fa1884368ed23ff6a3df9bf56d6c5
HemangGour <hemanggour547@gmail.com>
2026-01-31 17:30:59 +0530

    Added endure_bucket_exists to create a storage bucket if the STORAGE_BACKEND is set to s3 and if the bucket don't exists

--- BACKEND/music/apps.py
+++ BACKEND/music/apps.py
@@ -1,6 +1,15 @@
 from django.apps import AppConfig
+from django.conf import settings
+
+from utils.storage import ensure_bucket_exists
 
 
 class MusicConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "music"
+
+    def ready(self):
+        if settings.STORAGE_BACKEND == "s3":
+            ensure_bucket_exists(
+                settings.AWS_STORAGE_BUCKET_NAME, settings.AWS_S3_REGION_NAME
+            )
--- BACKEND/utils/storage.py
+++ BACKEND/utils/storage.py
@@ -1,3 +1,5 @@
+import boto3
+from botocore.exceptions import ClientError
 from django.conf import settings
 from storages.backends.s3boto3 import S3Boto3Storage
 
@@ -17,3 +19,39 @@ class PublicS3Boto3Storage(S3Boto3Storage):
             url = url.replace(internal_endpoint, public_endpoint)
 
         return url
+
+
+def ensure_bucket_exists(
+    bucket_name: str,
+    region: str | None = None,
+):
+    s3 = boto3.client(
+        "s3",
+        endpoint_url=settings.AWS_S3_ENDPOINT_URL,
+        aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
+        aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
+        region_name=settings.AWS_S3_REGION_NAME,
+    )
+
+    try:
+        s3.head_bucket(Bucket=bucket_name)
+        print(f"Bucket '{bucket_name}' already exists")
+
+    except ClientError as e:
+        error_code = int(e.response["Error"]["Code"])
+
+        # Bucket does not exist
+        if error_code == 404:
+            create_params = {"Bucket": bucket_name}
+
+            # AWS requires region for non us-east-1
+            if region:
+                create_params["CreateBucketConfiguration"] = {
+                    "LocationConstraint": region
+                }
+
+            s3.create_bucket(**create_params)
+            print(f"Bucket '{bucket_name}' created")
+
+        else:
+            raise

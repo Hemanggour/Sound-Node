e3ce2f9be75e48d7611bc7eaa0a2fb14a726fc6e
HemangGour <hemanggour547@gmail.com>
2026-02-26 13:44:38 +0530

    Fixed frontend env issue

--- FRONTEND/vite.config.ts
+++ FRONTEND/vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import dotenv from "dotenv";
+import path from "path";
 
-// https://vite.dev/config/
+dotenv.config({
+  path: path.resolve(__dirname, ".env"),
+  override: true,
+});
+
 export default defineConfig({
   plugins: [react()],
-})
+});
